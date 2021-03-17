3bbd0c4fb7ac5af86a3d5090a1ca748d424fb15c
Cody Wiseman <cwiseman09@yahoo.com>
2021-03-17 11:23:09 -0700

    step 3

---
+++ algo-ds-udemy/problem-solving.js
@@ -0,0 +1,23 @@
+// Algorithm - a PROCESS or a SET OF STEPS to accomplish a certain task
+
+//* PROBLEM SOLVING //
+
+  //* 1.Understand the problem
+    // Can I restate the problem in my own words?
+    // What are the inputs that go into the problem
+    // What are the outputs that should come from the solution
+    // Can the outputs be determined from the inputs? In other words,
+      // do I have enough info to solve the problem?
+    // How should I label the important pieces of data that are part of the problem
+
+  //* 2 Exploring concrete examples
+    // Coming up with examples can help you understand the problem better
+    // Start with simple examples
+    // Progress to more complex examples
+    // Explore examples with empty inputs
+    // Explore examples with invalid inputs
+
+  //* 3. Break it down
+    // Explicitly write out the steps you need to take
+
+  //* 4 Solve/Simplify
