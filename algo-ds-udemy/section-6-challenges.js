01417ccfee5b136b88074a242362f6cf12cfaf73
Cody Wiseman <cwiseman09@yahoo.com>
2021-03-31 21:13:32 -0700

    minSubArrayLen

--- algo-ds-udemy/section-6-challenges.js
+++ algo-ds-udemy/section-6-challenges.js
@@ -111,3 +111,30 @@ function maxSubarray(array, num) {
   }
   return max;
 }
+
+// Write a function called minSubarrayLen which accepts two parameters - an array of positive
+// integers and a positive integer. The function should return the minimal lenght of a contiguous
+// subarray of which is greater than or equal to the integer passed to the function. If there isn't
+// one, return 0 instead.
+
+function minSubArrayLen(nums, sum) {
+  let total = 0;
+  let start = 0;
+  let end = 0;
+  let minLen = Infinity;
+  while(start < nums.length) {
+    if(total < sum && end < nums.length) {
+      total += nums[end];
+      end++
+    }
+    else if(total >= sum) {
+      minLen = Math.min(minLen, end - start);
+      total -= nums[start];
+      start++;
+    }
+    else {
+      break;
+    }
+  }
+  return minLen === Infinity ? 0 : minLen;
+}
